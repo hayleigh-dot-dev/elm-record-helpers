e7a589f651643f6a7042d8a7e369aec7ea4c4691
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2019-11-26 23:05:52 +0000

    :wrench: Write deploy script to build and push to gh-pages.

---
+++ deploy.js
@@ -0,0 +1,29 @@
+const Bundler = require('parcel-bundler')
+const Path = require('path')
+const GitHub = require('gh-pages')
+
+process.env.NODE_ENV = 'production'
+
+function deploy (entry) {
+  const parcel = new Bundler(entry, {
+    outDir: './gh-pages',
+    outFile: 'index.html',
+    publicUrl: '/elm-record-helper',
+    watch: false,
+    cache: false,
+    contentHash: false,
+    minify: true,
+    target: 'browser',
+    hmr: false,
+    sourceMaps: false
+  })
+
+  parcel.bundle().then(() => {
+    GitHub.publish('gh-pages', {
+      branch: 'master',
+      message: ':tada: Deploy site.'
+    }, err => err ? console.error(err) : console.log('🎉 Site deployed!'))
+  })
+}
+
+deploy(Path.join(__dirname, './src/index.html'))
